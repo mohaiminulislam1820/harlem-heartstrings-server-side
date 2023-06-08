0c95d3f4cbf9bc6e2ade217059bc27c5fb28bed0
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-06-08 09:17:10 +0600

    added mongodb to the api server

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log('listening on port ', PORT);
+})
+
+app.get('/', (req, res) => {
+    res.send('Harlem Heartstrings api server running....');
+})
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@crud-practice.heeny6h.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
